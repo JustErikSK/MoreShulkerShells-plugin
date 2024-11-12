ff8965c544979d9cca679cd71d8e8729f39642d0
juste <sebo.erik@gmail.com>
2024-11-12 16:42:41 +0100

    updated to 1.21.3, added necessary code, added plugin.yml for spigot

--- src/main/java/me/minecraft/plugin/moreShulkerShells/MoreShulkerShells.java
+++ src/main/java/me/minecraft/plugin/moreShulkerShells/MoreShulkerShells.java
@@ -1,17 +1,30 @@
 package me.minecraft.plugin.moreShulkerShells;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MoreShulkerShells extends JavaPlugin {
+public final class MoreShulkerShells extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "MoreShulkerShells >> Plugin has been enabled!");
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void shulkerDeath(EntityDeathEvent e) {
+        LivingEntity ent = e.getEntity();
+
+        if (ent.getType() == EntityType.SHULKER) {
+            e.getDrops().add(new ItemStack(Material.SHULKER_SHELL, 3));
+        }
     }
 }
