10a913822805be58e02d4c5288b87c310ed20431
justerik <sebo.erik@gmail.com>
2024-12-09 17:42:51 +0100

    Added config file, added code for changeable drop amounts, needs testing

--- src/main/java/me/minecraft/plugin/moreShulkerShells/MoreShulkerShells.java
+++ src/main/java/me/minecraft/plugin/moreShulkerShells/MoreShulkerShells.java
@@ -3,6 +3,7 @@ package me.minecraft.plugin.moreShulkerShells;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
@@ -17,14 +18,24 @@ public final class MoreShulkerShells extends JavaPlugin implements Listener {
     public void onEnable() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "MoreShulkerShells >> Plugin has been enabled!");
         this.getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+
+        FileConfiguration config = this.getConfig();
+        config.addDefault("shulker_shell_drop_amount", 3);
     }
 
     @EventHandler
     public void shulkerDeath(EntityDeathEvent e) {
         LivingEntity ent = e.getEntity();
 
+        int ss_am = this.getConfig().getInt("shulker_shell_drop_amount", 3);
+
+        if (ss_am > 10 || ss_am < 1) {
+            ss_am = 3;
+        }
+
         if (ent.getType() == EntityType.SHULKER) {
-            e.getDrops().add(new ItemStack(Material.SHULKER_SHELL, 3));
+            e.getDrops().add(new ItemStack(Material.SHULKER_SHELL, ss_am));
         }
     }
 }
